2f275729fb9f096463a95b8869590dc4d0a8f183
Thigo <thiago.l17@yahoo.com>
2020-12-23 19:32:36 -0300

    done script

--- dinosaur/js/script.js
+++ dinosaur/js/script.js
@@ -1,6 +1,7 @@
 const dino = document.querySelector('.dino');
 const background = document.querySelector('.background');
 let isJumping = false;
+let position = 0;
 
 let handleKeyUp = (event) => {
   if (event.keyCode === 32) {
@@ -11,7 +12,6 @@ let handleKeyUp = (event) => {
 }
 
 let jump = () => {
-  let position = 0;
   isJumping = true;
 
   let upInterval = setInterval(() => {
@@ -38,10 +38,28 @@ let jump = () => {
 let createCactus = () => {
   const cactus = document.createElement('div');
   let cactusPosition = 1000;
+  let randomTime = Math.random() * 6000;
 
   cactus.classList.add('cactus');
   cactus.style.left = 1000 + 'px';
   background.appendChild(cactus);
+
+  let leftInterval = setInterval(() => {
+    if (cactusPosition < -60) {
+      clearInterval(leftInterval);
+      background.removeChild(cactus);
+
+    } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60 ) {
+      clearInterval(leftInterval);
+      document.body.innerHTML = '<h1 class="game-over">Fim de Jogo</h1>'
+
+    } else {
+      cactusPosition -= 10;
+      cactus.style.left = cactusPosition + 'px';
+    }
+  }, 20);
+
+  setTimeout(createCactus, randomTime);
 }
 
 createCactus();
