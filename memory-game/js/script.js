7a4cac84da9549594152c46ee0ff4556c41bc8a7
Thigo <thiago.l17@yahoo.com>
2020-12-24 02:20:25 -0300

    fix bug flipcard

--- memory-game/js/script.js
+++ memory-game/js/script.js
@@ -1,8 +1,12 @@
 const cards = document.querySelectorAll('.card');
 let hasFlippedCard = false;
 let firstCard, secondCard;
+let lockBoard = false
 
 function flipCard() {
+  if (lockBoard) return;
+  if (firstCard === this) return;
+
   this.classList.toggle('flip');
   if (!hasFlippedCard) {
     hasFlippedCard = true;
@@ -22,6 +26,22 @@ function checkForMatch() {
   }
 
   unflipCards();
+}
+
+function disableCard() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCards() {
+  lockBoard = true
+
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+
+    lockBoard = false 
+  }, 1500);
 }
 
 cards.forEach((card) => {
