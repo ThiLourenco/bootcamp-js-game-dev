a0448c7daad04c3f66913e26f712f81a03e520a6
Thigo <thiago.l17@yahoo.com>
2020-12-24 01:59:21 -0300

    creating script

--- memory-game/js/script.js
+++ memory-game/js/script.js
@@ -1,7 +1,27 @@
 const cards = document.querySelectorAll('.card');
+let hasFlippedCard = false;
+let firstCard, secondCard;
 
 function flipCard() {
   this.classList.toggle('flip');
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+    return;
+  }
+
+  secondCard = this;
+  hasFlippedCard = false;
+  checkForMatch();
+}
+
+function checkForMatch() {
+  if (firstCard.dataset.card === secondCard.dataset.card) {
+    disableCard();
+    return;
+  }
+
+  unflipCards();
 }
 
 cards.forEach((card) => {
